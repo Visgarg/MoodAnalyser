ae269ff208e54b5bd3db1d8e6503b9d8467311d5
Vishal Garg <vishugarg10004@gmail.com>
2020-10-11 14:19:38 +0530

    Add project files.

---
+++ MoodAnalyserMSTest/UnitTest1.cs
@@ -0,0 +1,43 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyser;
+using System.Runtime.InteropServices;
+
+namespace MoodAnalyserMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        MoodAnalyserClass moodAnalyserClass;
+        [TestInitialize]
+        public void Setup()
+        {
+             moodAnalyserClass = new MoodAnalyserClass("I am in sad mood");
+        }
+        [TestMethod]
+        public void GivenSadMoodShouldReturnSAD()
+        {
+            //Arrange
+            string expected = "SAD";
+             //string message = "I am in Sad Mood";
+            //MoodAnalyserClass moodAnalyserClass = new MoodAnalyserClass(message);
+
+            //Act
+            string actual = moodAnalyserClass.AnalyseMood();
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void GivenAnyMoodShouldReturnHAPPY()
+        {
+            //Arrange
+            string expected = "HAPPY";
+            //string message = "I am in any mood";
+            //Add
+            string actual= moodAnalyserClass.AnalyseMood();
+            //Assert
+            Assert.AreEqual(expected, actual);
+            
+        }
+    }
+}
